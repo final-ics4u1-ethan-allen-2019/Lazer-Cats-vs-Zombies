ea5781962af5b8ceaa7c9de95fcff718efab87f5
Ethan918273 <ethan918273@gmail.com>
2019-05-17 10:52:39 -0400

    Added some general files

--- src/engine/Draw.java
+++ src/engine/Draw.java
@@ -1,6 +1,9 @@
 package engine;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 
 public class Draw {
 
@@ -16,8 +19,32 @@ public class Draw {
 
     // Drawing methods
 
-    public static void ellipse() {
+    public static void setFont(Font font) {
+        graphics.setFont(font);
+    }
 
+    public static void setFill(Paint paint) {
+        graphics.setFill(paint);
+    }
+
+    public static void ellipse(double x, double y, double width, double height) {
+        graphics.fillOval(x, y, width, height);
+    }
+
+    public static void rect(double x, double y, double width, double height) {
+        graphics.fillRect(x, y, width, height);
+    }
+
+    public static void drawImage(Image image, double x, double y, double width, double height) {
+        graphics.drawImage(image, x, y, width, height);
+    }
+
+    public static void drawImage(Image image, double x, double y) {
+        graphics.drawImage(image, x, y, image.getWidth(), image.getWidth());
+    }
+
+    public static void drawText(String text, double x, double y) {
+        graphics.fillText(text, x, y);
     }
 
 }
