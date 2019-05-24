c6c2e3bd9071aec497de70c938bdbc36300968ce
elbattery <allen.lu@hotmail.ca>
2019-05-24 13:02:51 -0400

    OOps forgot to do the dev branch?

--- src/engine/scenes/GameScene.java
+++ src/engine/scenes/GameScene.java
@@ -1,4 +1,11 @@
 package engine.scenes;
 
+import java.util.ArrayList;
+
 public class GameScene {
+
+    //int array for tile map generation
+    ArrayList<ArrayList<Integer>> intMap;
+
+
 }
--- src/game/Entity.java
+++ src/game/Entity.java
@@ -1,9 +1,11 @@
 package game;
 
 import engine.GameObject;
+import engine.scripts.SpriteRenderer;
 
 public class Entity extends GameObject {
 
+    SpriteRenderer renderer;
     public Entity() {
 
     }
